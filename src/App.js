20a337add18edd3cf3e82b63f5bf30cf2337198a
Caiosev <kaiosev9@gmail.com>
2022-08-31 10:39:34 -0300

    config gist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch, HashRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Foods from './pages/Foods';
 import Drinks from './pages/Drinks';
@@ -12,20 +12,20 @@ import RecipeInProgress from './pages/RecipeInProgress';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route path="/foods/:id/in-progress" component={ RecipeInProgress } />
-        <Route path="/foods/:id" component={ RecipeDetails } />
-        <Route path="/foods" component={ Foods } />
-        <Route path="/drinks/:id/in-progress" component={ RecipeInProgress } />
-        <Route path="/drinks/:id" component={ RecipeDetails } />
-        <Route path="/drinks" component={ Drinks } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/done-recipes" component={ DoneRecipes } />
-        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+        <Route exact path="/" component={Login} />
+        <Route path="/foods/:id/in-progress" component={RecipeInProgress} />
+        <Route path="/foods/:id" component={RecipeDetails} />
+        <Route path="/foods" component={Foods} />
+        <Route path="/drinks/:id/in-progress" component={RecipeInProgress} />
+        <Route path="/drinks/:id" component={RecipeDetails} />
+        <Route path="/drinks" component={Drinks} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/done-recipes" component={DoneRecipes} />
+        <Route path="/favorite-recipes" component={FavoriteRecipes} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
